71195edf40ce4e0ce4db4f9dc7347b595869453e
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-04-02 19:30:46 +0200

    Code cleanup, basic styling pages added

--- hello-world-web/main.go
+++ hello-world-web/main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 // const cannot be overriden, and var can
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
 
 // main is the main application function
 func main() {
@@ -30,15 +19,4 @@ func main() {
 	// start server on port :8080
 	// _ = means if there is an error, I don't care
 	_ = http.ListenAndServe(portNumber, nil)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	// parse the template
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
 }
