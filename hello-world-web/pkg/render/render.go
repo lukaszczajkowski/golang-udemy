ac3d5e546cccf729bc1c210d6f1f35bcb3429ad9
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-04-03 18:52:35 +0200

    Working with Layouts and building a template cache

--- hello-world-web/pkg/render/render.go
+++ hello-world-web/pkg/render/render.go
@@ -4,16 +4,63 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
+
+var functions = template.FuncMap{}
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	_, err := RenderTemplateTest(w)
+
+	if err != nil {
+		fmt.Println("Error getting template cache:", err)
+	}
 	// parse the template
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	err = parsedTemplate.Execute(w, nil)
 
 	if err != nil {
 		fmt.Println("error parsing template:", err)
 		return
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return myCache, nil
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently", page)
+		// ts -> template set
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+
+		if err != nil {
+			return myCache, nil
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+
+		if err != nil {
+			return myCache, nil
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+
+			if err != nil {
+				return myCache, nil
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
