5e74b3a2adbfa013ab15abad7503760e5ca5025e
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-04-03 20:38:33 +0200

    Sharing data with templates

--- hello-world-web/pkg/handlers/handlers.go
+++ hello-world-web/pkg/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/lukaszczajkowski/golang-udemy/pkg/config"
+	"github.com/lukaszczajkowski/golang-udemy/pkg/models"
 	"github.com/lukaszczajkowski/golang-udemy/pkg/render"
 	"net/http"
 )
@@ -9,7 +10,7 @@ import (
 // Repo the repository used by the handlers
 var Repo *Repository
 
-// Repository is thr repository type
+// Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
 }
@@ -28,10 +29,17 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello again."
+
+	//send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
--- hello-world-web/pkg/render/render.go
+++ hello-world-web/pkg/render/render.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/lukaszczajkowski/golang-udemy/pkg/config"
+	"github.com/lukaszczajkowski/golang-udemy/pkg/models"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,8 +20,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
@@ -38,7 +44,9 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	buf := new(bytes.Buffer)
 
-	_ = t.Execute(buf, nil)
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
 
